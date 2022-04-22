f6224fc68c5a8553dbbcba5973bf8c2e0a099733
Dipsaint1 <ayodejioladapo96@gmail.com>
2022-04-22 22:08:11 +0100

    Started a Currency Conerter Project

---
+++ app.js
@@ -0,0 +1,59 @@
+const fromCurrency = document.querySelector(".from-currency");
+const toCurrency = document.querySelector(".to-currency");
+const amount = document.getElementById("amount");
+
+
+
+class CurrencyConverter{
+  constructor(){
+    window.addEventListener("DOMContentLoaded", this.loadWindow);
+  }
+
+
+  loadWindow(){
+    CurrencyConverter.currencies();
+  }
+
+  static getKeys(keys, currField){
+    keys.sort();
+    keys.forEach(key => {
+      const option = document.createElement("option");
+      option.setAttribute("value", key);
+      option.innerText = key;
+      currField.append(option);
+    });
+  }
+
+  static loadFromCurrencyField(data){
+    const keys = Object.keys(data);
+    CurrencyConverter.getKeys(keys, fromCurrency);
+  }
+
+  static loadToCurrencyField(data){
+    const keys = Object.keys(data);
+    CurrencyConverter.getKeys(keys, toCurrency);
+  }
+
+  // Functions
+  static currencies(){
+    fetch("https://api.vatcomply.com/currencies")
+      .then((response) => {
+        if(!response.ok) throw new Error(response.status);
+        return response.json();
+      })
+      .then((data) => {
+        CurrencyConverter.loadFromCurrencyField(data);
+        CurrencyConverter.loadToCurrencyField(data);
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+
+  // Event Listeners
+
+  // 
+}
+
+
+new CurrencyConverter();
